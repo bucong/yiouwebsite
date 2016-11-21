e5a026aedd0dc2fd607f6453e739ad59de3ea632
bucong <bucong@sodo-tech.com>
2016-11-21 11:27:52 +0800

    宜欧官网

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function() {
+	$('.index .intro .tab div').click(function(){
+		$(this).addClass('active').siblings().removeClass('active');
+		$('.index .intro li').eq($(this).index()).css('display','flex').siblings().css('display','none');
+	})
+	$('.contact-us li').click(function(){
+		$(this).addClass('active').siblings().removeClass('active');
+		$('.contact-us iframe').attr('src','map/map'+$(this).index()+'.html');
+	})
+})
